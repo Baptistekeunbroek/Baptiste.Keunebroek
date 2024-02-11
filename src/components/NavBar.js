1ef568763d733cd79fb614bb4c04dffc66431b47
Baptiste Keunebroek <baptiste.keunebroek@efrei.net>
2024-02-11 19:30:07 +0100

    NavBar modif + style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,38 +3,35 @@ import "../style/NavBar.css";
 
 const NavBar = () => {
   return (
-    <div className="container">
+    <div className="containerNavBar">
       <div className="Main_Container">
+        <div className="Links">
+          <NavLink
+            id="link"
+            style={{ textDecoration: "none", color: "black", fontSize: 25 }}
+            to="/"
+          >
+            Baptiste Keunebroek
+          </NavLink>
+        </div>
         <div className="Main_elements">
-          <div className="MyName">Baptiste Keunebroek</div>
-
-          <div className="Links">
-            <NavLink
-              id="home"
-              style={{ textDecoration: "none", color: "black" }}
-              to="/"
-            >
-              HOME
-            </NavLink>
-          </div>
-
-          <div className="Links">
+          <div className="Links2">
             <NavLink
               id="link"
-              style={{ textDecoration: "none", color: "black" }}
+              style={{ textDecoration: "none", color: "black", fontSize: 30 }}
               to="/projects"
             >
-              PROJECTS
+              <i className="fas fa-folder-open"></i>
             </NavLink>
           </div>
 
-          <div className="Links">
+          <div className="Links2">
             <NavLink
               id="link"
-              style={{ textDecoration: "none", color: "black" }}
+              style={{ textDecoration: "none", color: "black", fontSize: 30 }}
               to="/link"
             >
-              LINK
+              <i className="fas fa-link"></i>
             </NavLink>
           </div>
         </div>
