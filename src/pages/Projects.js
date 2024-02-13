fe577185cd5f88add0f89c85ca6329637498c897
Baptiste Keunebroek <baptiste.keunebroek@efrei.net>
2024-02-13 20:11:30 +0100

    Delete no use files + project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,30 +4,36 @@ import "../style/Projects.css";
 
 const Projects = () => {
   return (
-    <div className="projects_container">
-      <div className="first_project">
-        <NavLink
-          className="Project_UNO"
-          to="https://github.com/VengaGames/Uno"
-        ></NavLink>
-        <div className="project_text">
-          <h1>UNO Project</h1>
-          This is my UNO project : text text text
+    <div className="projects_page">
+      <div className="projects_container">
+        <div className="first_project">
+          <NavLink
+            className="Project_UNO"
+            to="https://github.com/VengaGames/Uno"
+          ></NavLink>
+          <div className="project_text_UNO">
+            <h1>UNO Project</h1>
+            Me and my friends made a remake of the famous game, using JavaScript
+            and Node.js
+          </div>
         </div>
-      </div>
-      <div className="second_project">
-        <div className="project_text">
-          <h1>BlindTest Project</h1>
-          This is my BlindTest project : text text text
+        <div className="second_project">
+          <NavLink
+            className="Project_BlindTest"
+            to="https://github.com/Malo-LC/BlindTest"
+          ></NavLink>
+          <div className="project_text_BlindTest">
+            <h1>BlindTest Project</h1>
+            Here is a school project, an online BlindTest that allows you to
+            play with you friends ! Made with React, Tailwind, Node.js ans
+            Socket.io.
+          </div>
         </div>
-        <NavLink
-          className="Project_BlindTest"
-          to="https://github.com/Malo-LC/BlindTest"
-        ></NavLink>
+        <div className="positionedTextProjects">Projects</div>
+        <div className="positionedTextProjects">Projects</div>
+        <div className="positionedTextProjects">Projects</div>
       </div>
-      <div className="positionedTextProjects">Projects</div>
-      <div className="positionedTextProjects">Projects</div>
-      <div className="positionedTextProjects">Projects</div>
+      <div className="projects_container_bottom"></div>
     </div>
   );
 };
